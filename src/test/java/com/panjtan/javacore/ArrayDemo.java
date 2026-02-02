508e6a744f041d3ca22344ee180c926bd5c5bfb4
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2026-02-02 08:53:07 +0530

    Array code added

--- src/test/java/com/panjtan/javacore/ArrayDemo.java
+++ src/test/java/com/panjtan/javacore/ArrayDemo.java
@@ -12,24 +12,55 @@ public class ArrayDemo {
         int arr[] = new int[10];
         String[] arrStr = new String[5];
         boolean[] arrBl = new boolean[5];
+        char[] arrC = new char[5];
+//        System.out.println(Arrays.toString(arrC));
+//        System.out.println(arrC[0]);
 
 //        Create an array with available data
-        String []fruits = {"Apple", "Banana", "Guava", "Date"};
+        String []fruits = {"Apple", "Banana", "Guava", "Date"}; //length = 4
 
-        System.out.println(arr[0]);
-//        for (int i : arr)
-//            System.out.println(i);
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(arrStr));
-        System.out.println(Arrays.toString(arrBl));
+//        for (int i = 0; i < fruits.length; i++) {
+//            System.out.println(fruits[i]);
+//        }
+//        System.out.println(arr[0]);
+////        for (int i : arr)
+////            System.out.println(i);
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arrStr));
+//        System.out.println(Arrays.toString(arrBl));
+//
+//        System.out.println(Arrays.toString(fruits));
+//
+////        array Manipulation
+//        System.out.println(fruits[0]);
+//        fruits[0] = "Cherry";
+//        System.out.println(Arrays.toString(fruits));
+//        fruits[3] = "Nut";
+//        System.out.println(Arrays.toString(fruits));
 
+//
+//        String[] newFruits = fruits.clone();
+//        System.out.println(Arrays.toString(newFruits));
+//        newFruits[0] = "PineApple";
+//        System.out.println("New Fruit: " + Arrays.toString(newFruits));
+//        System.out.printf("Original Fruit: " + Arrays.toString(fruits));
+//
+//        System.out.println("*".repeat(40));
+//        String[] fruits1 = fruits;
+//        fruits1[0] = "Cherry";
+//        System.out.println("New Fruit: " + Arrays.toString(fruits1));
+//        System.out.printf("Original Fruit: " + Arrays.toString(fruits));
+
+        Arrays.sort(fruits);
         System.out.println(Arrays.toString(fruits));
 
-//        array Manipulation
-        System.out.println(fruits[0]);
-        fruits[0] = "Cherry";
-        System.out.println(Arrays.toString(fruits));
-        fruits[4] = "Nut";
-        System.out.println(Arrays.toString(fruits));
+
+        int[] num2 = {1, 6, 7, 2, 3, 4, 10, 5, 8, 9};
+//        Arrays.sort(num2);
+//        System.out.println(Arrays.toString(num2));
+
+        Arrays.sort(num2, 2, 8);
+        System.out.println(Arrays.toString(num2));
+
     }
 }
